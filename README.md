603f70296a67242d60984956a7024f0d85163f2d
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2024-03-09 21:43:19 +0800

    Early implementation

---
+++ README.md
@@ -0,0 +1,7 @@
+# Infinite Scroll
+
+Continually renders a small number of rows as if they're the rows you would be looking at if all the rows were rendered.
+
+This works by continually moving the rows once they're out of view to where you are about to scroll to.
+
+Last holiday I studied someone else's VanillaJS that demonstrated this and now I'm doing it here but probably slower because it's using a framework.
---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react'
+import { useState } from 'react'
+import './App.css'
+
+const settings = {
+  rowHeight: 20,
+  numRows: 10000,
+  numVisibleRows: 100
+}
+
+const rows: { id: number }[] = []
+
+for (let i = 0; i < settings.numRows; i++) {
+  rows.push({ id: i })
+}
+
+const Row: React.FC<{ id: number, i: number, yScroll: number }> = ({ id, i, yScroll }) => {
+  // const yPos = rowHeight
+
+  return (
+    <div className="row" style={{
+      top: yScroll + (i * settings.rowHeight)
+    }}>
+      <p>{id}</p>
+    </div>
+  )
+}
+
+const totalHeight = settings.rowHeight * settings.numRows
+
+function App() {
+  const [yScroll, setYScroll] = useState(0)
+  
+  React.useEffect(() => {
+    addEventListener('scroll', () => {
+      setYScroll(scrollY)
+    })
+  })
+
+  const iRowInView = Math.ceil(yScroll / settings.rowHeight)
+
+  const rowNodes = []
+
+  for (let i = 0; i < settings.numVisibleRows; i++) {
+    rowNodes.push(<Row key={i} {...rows[iRowInView + i]} i={i} yScroll={yScroll} />)
+  }
+
+  return (
+    <>
+      <div className="rowContainer" style={{ height: totalHeight }}>
+        { rowNodes }
+      </div>
+    </>
+  )
+}
+
+export default App
