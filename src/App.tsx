cf1d271408feb9a6a8825750f2414188a4572c41
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2024-03-10 13:39:29 +0800

    Add note about serious bug

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,8 @@ const Row: React.FC<{ id: number, i: number, top: number, active: boolean }> = (
 
 const totalHeight = settings.rowHeight * settings.numRows
 
-const createRows = () => {
+/** Create an array representing the rendered rows, not the data source's rows */
+const createVisibleRows = () => {
   const rows = []
 
   for (let i = 0; i < settings.numVisibleRows; i++) {
@@ -40,89 +41,56 @@ const createRows = () => {
   return rows
 }
 
-const vRows = createRows()
-
 function App() {
   const [yScroll, setYScroll] = useState(0)
   const [vScreen, setVScreen] = useState(visualViewport.height)
-  // const [vRows, setVRows] = useState(createRows())
+  /** Visible rows, the rendered rows, to roll over the actual rows */
+  const [vRows, setVRows] = useState(createVisibleRows())
   
   React.useEffect(() => {
     addEventListener('scroll', () => setYScroll(scrollY))
     addEventListener('resize', () => setVScreen(visualViewport.height))
   })
 
-  const iRowInView = Math.ceil(yScroll / settings.rowHeight)
+  const iRowInView = Math.floor(yScroll / settings.rowHeight)
 
   console.log('row in view', iRowInView)
 
-  const numRowsOnScreen = Math.ceil(vScreen / settings.rowHeight)
+  const numRowsOnScreen = Math.floor(vScreen / settings.rowHeight)
   const numRowsWithOffsets = numRowsOnScreen * 2
 
-  // const insertAbove = yScroll - vScreen
-  // const insertBelow = yScroll + vScreen + (vScreen / 2)
-  // const insertBefore = Math.ceil(iRowInView - (numRowsOnScreen / 2))
-  // const insertBefore = Math.ceil(iRowInView - (numRowsOnScreen / 2))
-  // const insertAfter = Math.ceil(iRowInView + (numRowsOnScreen / 2))
-  // const vRowInViewTop = Math.ceil(iRowInView % vRows.length)
-
-  const centerRow = Math.ceil(iRowInView + (numRowsOnScreen / 2))
+  const centerRow = Math.floor(iRowInView + (numRowsOnScreen / 2))
   const activeIds = []
-  for (let nI = 0; nI < numRowsOnScreen; nI++) {
-    // activeIds.push(mod(Math.max(iRowInView + nI, 0), vRows.length))
-    // activeIds.push(centerRow - (numRowsWithOffsets / 2) + nI)
-    activeIds.push(Math.floor(centerRow - (numRowsOnScreen / 2)) + nI)
-    // activeIds.push((iRowInView + nI) % vRows.length)
+  for (let nI = 0; nI < numRowsWithOffsets; nI++) {
+    activeIds.push(Math.floor(centerRow - (numRowsWithOffsets / 2)) + nI)
   }
 
   console.log('active ids', activeIds)
   console.log('y scroll:', yScroll, 'iRowInView', iRowInView, 'centerRow:', centerRow, rowsData[centerRow].id)
   console.log('num rows on screen:', numRowsOnScreen, 'num visible:', settings.numVisibleRows)
 
-  for (let i = 0; i < vRows.length; i++) {
-    // 
-    
+  // NOTE: There is a serious bug where first row (id: 0) is not rendered or is not considered active
+
+  for (let i = 0; i < vRows.length; i++) {   
     const vRow = vRows[(iRowInView + i) % vRows.length]
-    const row = rowsData[Math.ceil(((iRowInView + (numRowsOnScreen / 2)) - (vRows.length / 2)) + i)]
+    const row = rowsData[Math.floor((centerRow - (vRows.length / 2)) + i)]
     if (!row) continue
-    const active = !yScroll || activeIds.includes(row.id)
-    if (!active) {
+    const active = activeIds.includes(row.id)
+    if (!yScroll || !active) {
       vRow.top = row.id * settings.rowHeight
       vRow.rowId = row.id
     }
 
-    // const target = mod(Math.ceil(iRowInView + i), vRows.length)
-    // // const target = i
-    // const vRow = vRows[target]
-    // const row = rows[Math.ceil(iRowInView - vRows.length / 2) + target]
-    // if (!row) continue
-    // const { top } = vRow
-    // // const updateTop = !yScroll || (yScroll && !top) || (top < insertAbove) || (top > insertBelow)
-    // // const updateTop = !yScroll || (top < insertAbove) || (top > insertBelow)
-    // const updateTop = !yScroll || row.id < insertBefore - numRowsOnScreen || row.id > insertAfter - numRowsOnScreen
-    // // console.log('update?', top, insertAbove, insertBelow, updateTop)
-    // console.log('update?', top, row.id, insertBefore, insertAfter, updateTop)
-    // if (updateTop) {
-      
-    //   // vRow.top = (iRowInView + target) * settings.rowHeight
-    //   vRow.top = row.id * settings.rowHeight
-    //   vRow.rowId = row.id
-      
-    //   console.log('vRow', i, iRowInView, target, vRow, row, top)
-    // } else {
-    //   console.log('vRow', i, iRowInView, target, vRow, 'x', top)
-    // }
-
     vRow.active = active
   }
-  console.log('mouse', vScreen, yScroll) //, insertAbove, insertBelow)
+  console.log('mouse', vScreen, yScroll)
   console.log('active & non', vRows.filter(vR => vR.active).length, 'VS', vRows.filter(vR => !vR.active).length)
   console.log('y first active', document.querySelectorAll('[data-active=a]')[1])
   console.log('y first inactive', document.querySelector('[data-active=b]'))
 
   // console.log('vRows', vRows)
 
-  const rowNodes = vRows.map((vRow, i) => <Row key={i} i={i} id={vRow.rowId} top={vRow.top} active={vRow.active} />)
+  const rowNodes = vRows.map((vRow, i) => (vRow.top > 0 || i === 0) && <Row key={i} i={i} id={vRow.rowId} top={vRow.top} active={vRow.active} />)
 
   return (
     <>
